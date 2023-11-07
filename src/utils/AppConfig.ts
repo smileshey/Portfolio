9c5e3fb82711984305c9865c9e0a2b68f20fcee4
smileshey <rtvpdx@gmail.com>
2023-11-07 09:06:15 -0800

    changed author of posts

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
+  site_name: 'Ryan\'s Portfolio',
+  title: 'Ryan\'s Portfolio',
+  description: 'Built with Astro using React and Tailwind CSS',
+  author: 'Ryan',
   locale_region: 'en-us',
   locale: 'en',
 };
