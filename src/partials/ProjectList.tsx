4aee69ee6b3eac2576ee4b63a4ccd18881cb5938
smileshey <rtvpdx@gmail.com>
2025-02-13 09:52:34 -0800

    added walkability project

--- src/partials/ProjectList.tsx
+++ src/partials/ProjectList.tsx
@@ -16,21 +16,35 @@ const ProjectList = () => (
   >
     <div className="flex flex-col gap-6">
       <Project
-          name="A Predictive Approach to Construction Cost Estimates"
-          description=" 
-          Passenger rail is a cornerstone of urban mobility. Given the high initial costs and financial risks associated with rail infrastructure projects, it's essential to provide stakeholders with clear cost estimates. This app introduces a construction cost estimation model that was trained on the data from existing rail projects to forecast the overall cost to build new passenger rail lines.        "
-          link="https://buildmoretrains.streamlit.app/"
-          img={{ src: '/assets/images/train.png', alt: 'SmashIcons: train logo' }}
-          category={
-            <>
-              <Tags color={ColorTags.VIOLET}>Machine Learning</Tags>
-              <Tags color={ColorTags.SKY}>Python</Tags>
-            </>
-          }
-        />
+        name="A Better Walkscore Application using GeoSpatial Data"
+        description="Existing measures like Walkscore often rely too heavily on proximity to businesses, rather than by using pedestrian amenities directly, which can misrepresent an area’s walkability. My project takes a different approach, using Seattle’s publicly available GIS data to generate a walkability index for every point in the city."
+        link="https://whale-app-icxda.ondigitalocean.app/"
+        img={{ src: '/assets/images/park.png', alt: 'SmashIcons: park logo' }}
+        category={
+          <>
+            <Tags color={ColorTags.VIOLET}>GeoSpatial</Tags>
+            <Tags color={ColorTags.SKY}>JS</Tags>
+            <Tags color={ColorTags.SKY}>Python</Tags>
+          </>
+        }
+      />
+
+      <Project
+        name="A Predictive Approach to Construction Cost Estimates"
+        description="Passenger rail is a cornerstone of urban mobility. Given the high initial costs and financial risks associated with rail infrastructure projects, it's essential to provide stakeholders with clear cost estimates. This app introduces a construction cost estimation model that was trained on data from existing rail projects to forecast the overall cost of building new passenger rail lines."
+        link="https://buildmoretrains.streamlit.app/"
+        img={{ src: '/assets/images/train.png', alt: 'SmashIcons: train logo' }}
+        category={
+          <>
+            <Tags color={ColorTags.VIOLET}>Machine Learning</Tags>
+            <Tags color={ColorTags.SKY}>Python</Tags>
+          </>
+        }
+      />
+
       <Project
         name="Classifying Speech Data using CNNs"
-        description="This project uses machine learning to distinguish between human and synthetic speech extracted from several datasets of voice audio files. "
+        description="This project uses machine learning to distinguish between human and synthetic speech extracted from several datasets of voice audio files."
         link="https://github.com/smileshey/VoiceClass--NN"
         img={{
           src: '/assets/images/mic-dynamic-color.png',
@@ -44,21 +58,9 @@ const ProjectList = () => (
           </>
         }
       />
-      <Project
-        name="NLP Subreddit Classification"
-        description="This project aims to predict the classification of text from two online communities using natural language processing techniques. By analyzing the content of posts, the project can help gain insights into the distinct behavior and preferences of the communities"
-        link="https://github.com/smileshey/Subreddit-NLP"
-        img={{ src: '/assets/images/reddit.png', alt: 'Project Fire' }}
-        category={
-          <>
-            <Tags color={ColorTags.VIOLET}>Machine Learning</Tags>
-            <Tags color={ColorTags.EMERALD}>Sentiment Analysis</Tags>
-            <Tags color={ColorTags.SKY}>Python</Tags>
-          </>
-        }
-      />
     </div>
   </Section>
 );
 
 export { ProjectList };
+
